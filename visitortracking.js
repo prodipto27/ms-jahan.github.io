bd7a618d37d14161014957fea7e0146f9200f0e3
Md Sarwar Jahan Sabit <ssarwarjahan@gmail.com>
2019-12-15 01:01:28 +0600

    Update visitortracking.js

--- visitortracking.js
+++ visitortracking.js
@@ -1,19 +1,11 @@
-  var txt = "";
-	txt += "<p>Url: " + window.location.href + "</p>"
-	txt += "<p> Ip Address: " + userip;
-	txt += "<p>Browser CodeName: " + navigator.appCodeName + "</p>";
-	txt += "<p>Browser Name: " + navigator.appName + "</p>";
-	txt += "<p>Browser Version: " + navigator.appVersion + "</p>";
-	txt += "<p>Cookies Enabled: " + navigator.cookieEnabled + "</p>";
-	txt += "<p>Browser Language: " + navigator.language + "</p>";
-	txt += "<p>Browser Online: " + navigator.onLine + "</p>";
-	txt += "<p>Platform: " + navigator.platform + "</p>";
-	txt += "<p>User-agent header: " + navigator.userAgent + "</p>";
-	
-    Email.send({
-    SecureToken : "d768c56e-85e5-4366-a24e-43cc4e84ded8",
-    To : 'ssarwarjahan@gmail.com',
-    From : "visitor@sab1t.me",
-    Subject : "Visitor Found on sab1t.me",
-    Body : txt
-});
+var txt = "";
+	txt += "Url: " + window.location.href + "\n";
+	txt += "Ip Address: " + userip + "\n";
+	txt += "Browser CodeName: " + navigator.appCodeName + "\n";
+	txt += "Browser Name: " + navigator.appName + "\n";
+	txt += "Browser Version: " + navigator.appVersion + "\n";
+	txt += "Cookies Enabled: " + navigator.cookieEnabled + "\n";
+	txt += "Browser Language: " + navigator.language + "\n";
+	txt += "Browser Online: " + navigator.onLine + "\n";
+	txt += "Platform: " + navigator.platform + "\n";
+	txt += "User-agent header: " + navigator.userAgent + "\n";
