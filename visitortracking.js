aa5fda8766e5d778f7df07bba075790a1b534b23
Md Sarwar Jahan Sabit <ssarwarjahan@gmail.com>
2019-12-16 19:24:16 +0600

    Update visitortracking.js

--- visitortracking.js
+++ visitortracking.js
@@ -1,6 +1,5 @@
 var txt = "";
 	txt += "Url: " + window.location.href + "\n";
-	txt += "Ip Address: " + userip + "\n";
 	txt += "Browser CodeName: " + navigator.appCodeName + "\n";
 	txt += "Browser Name: " + navigator.appName + "\n";
 //	txt += "Browser Version: " + navigator.appVersion + "\n";
@@ -9,5 +8,4 @@ var txt = "";
 //	txt += "Browser Online: " + navigator.onLine + "\n";
 	txt += "Platform: " + navigator.platform + "\n";
 	txt += "User-agent header: " + navigator.userAgent + "\n";
-	
-
+	txt += "Ip Address: " + userip + "\n";
